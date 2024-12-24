a6115c84a100dd51099d5760d9dbc9fdafb90d03
Anh Dau <redbeanboy98@gmail.com>
2024-12-24 17:27:58 +0700

    improve: scroll to top in main menu

--- src/components/menu/main-menu.tsx
+++ src/components/menu/main-menu.tsx
@@ -1,19 +1,30 @@
 import {
   filteredCategoriesAtom,
+  focusedCategoryIdAtom,
   searchProductQueryAtom,
 } from "@/state/product";
 import { CategoryTemplate } from "@/types/product";
 import classNames from "classnames";
 import { useAtomValue } from "jotai";
 import isEmpty from "lodash/isEmpty";
+import { useEffect, useRef } from "react";
 import CategoryItem from "./category-item";
 
 const MainMenu = () => {
   const searchQuery = useAtomValue(searchProductQueryAtom);
   const filteredCategories = useAtomValue(filteredCategoriesAtom);
+  const focusedCategoryId = useAtomValue(focusedCategoryIdAtom);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      ref.current?.scrollTo({ top: 0, behavior: "instant" });
+    });
+  }, [focusedCategoryId]);
 
   return (
     <div
+      ref={ref}
       className={classNames("flex-1 overflow-y-auto", {
         hidden: !isEmpty(searchQuery),
       })}
