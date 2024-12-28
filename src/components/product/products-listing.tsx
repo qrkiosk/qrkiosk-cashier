520a6cba47b1f7136415681f6bc1b198a5868377
Anh Dau <redbeanboy98@gmail.com>
2024-12-28 15:47:42 +0700

    improve update avail logic

--- src/components/product/products-listing.tsx
+++ src/components/product/products-listing.tsx
@@ -108,8 +108,10 @@ ProductsListing.List = forwardRef(
 
                   {updateAvailabilityMode && (
                     <Switch
+                      checked={product.isStock}
                       onChange={(e) => {
                         console.log(product.id, e.target.checked);
+                        // TODO: Call API to update availability of product
                       }}
                     />
                   )}
